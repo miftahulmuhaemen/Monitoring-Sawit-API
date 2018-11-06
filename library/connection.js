b2825d24ce67c9e5c3d8442c74767488ac19e6f2
miftahulmuhaemen <miftahulmuhaemen@gmail.com>
2018-11-06 08:24:38 +0800

    Added

--- library/connection.js
+++ library/connection.js
@@ -1,20 +1,17 @@
-// var mysql = require('mysql')
-// var connection = mysql.createConnection({
-//   host : 'localhost',
-//   user : 'root',
-//   password : '',
-//   database : 'db_sawit'
-// })
-//
-// connection.connect(function (err) {
-//   if(err) throw err
-// })
+var mysql = require('mysql');
 
-var pg = require('pg')
-var conString = 'postgres://rivlunuwhujquj:e647f4cd9a0ee3e5c6fc027f62104b4d6c7bd7a1a6711de6846dc25dd9b22cf1@ec2-54-83-49-109.compute-1.amazonaws.com:5432/d40j83837ca2kp'
-var client = new pg.Client(conString)
-client.connect(function (err){
-  if(err) throw err
-})
+var connection = mysql.createConnection({
+    host: 'remotemysql.com',
+    port:3306,
+    user: '1pd7O3aHgB',
+    password: 'JvImBwqJwl',
+    database: '1pd7O3aHgB'
+});
 
-module.exports = client;
+connection.connect(function (err) {
+    if(err) throw err;
+});
+
+
+
+module.exports = connection;
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 5432,
-    bodyParser = require('body-parser')
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
 
-var routes = require('./routes')
-routes(app)
-
-app.listen(port)
+var routes = require('./routes');
+routes(app);
+app.listen(port);
+console.log("WEB API Running.......");
