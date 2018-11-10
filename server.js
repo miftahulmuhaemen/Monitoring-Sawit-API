97faa9ad077087c6f47f66cf03ff0fd507c6d5eb
miftahulmuhaemen <miftahulmuhaemen@gmail.com>
2018-11-11 00:03:26 +0800

    Cors disbanded

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     app = express(),
     port = process.env.PORT || 3000,
+    cors = require('cors'),
     bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
