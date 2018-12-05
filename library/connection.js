7f183fef78ed753e1449559f3d3666513ec39b38
miftahulmuhaemen <miftahulmuhaemen@gmail.com>
2018-12-05 20:23:57 +0800

    s

--- library/connection.js
+++ library/connection.js
@@ -1,20 +1,29 @@
 var mysql = require('mysql');
-require('dotenv').config();
 
+var connection = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: '12345678',
+    database: 'sawit'
+    
+    // host: process.env.DB_HOST,
+    // port: process.env.PORT,
+    // user: process.env.DB_USER,
+    // database: process.env.DB_DATABASE,
+    // password: process.env.DB_PASS,
+    // socketPath : '/cloudsql/${process.env.DB_INSTANCE_NAME'
 
-let config = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    socketPath : '/cloudsql/${process.env.DB_INSTANCE_NAME}`'
+    //     user: process.env.SQL_USER,
+//     database: process.env.SQL_DATABASE,
+//     password: process.env.SQL_PASSWORD,
+//   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`
 });
-
-var connection = mysql.createConnection(config);
 
 connection.connect(function (err) {
     if(err) throw err;
 });
+
 
 
 module.exports = connection;
